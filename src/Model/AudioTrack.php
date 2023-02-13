694eb8d4b77f4fed0d727d92ffbd3f50d385a173
LupusVII <jln.thirion@gmail.com>
2023-02-13 23:46:31 +0100

    Add statement for AudioTrack model

--- src/Model/AudioTrack.php
+++ src/Model/AudioTrack.php
@@ -15,4 +15,83 @@ class AudioTrack extends \WEM\UtilsBundle\Model\Model
      * @var string
      */
     protected static $strTable = 'tl_wem_audiotrack';
+
+    /**
+     * Generic statements format.
+     *
+     * @param string $strField    [Column to format]
+     * @param mixed  $varValue    [Value to use]
+     * @param string $strOperator [Operator to use, default "="]
+     *
+     * @return array
+     */
+    public static function formatStatement($strField, $varValue, $strOperator = '=')
+    {
+        try {
+            $arrColumns = [];
+            $t = static::$strTable;
+
+            switch ($strField) {
+                case 'pid':
+                    if (!$varValue || !\is_array($varValue)) {
+                        $varValue = [$varValue];
+                    }
+
+                    $arrColumns[] = sprintf("$t.pid IN('%s')", implode("','", $varValue));
+                break;
+
+                case 'search':
+                    $arrColumns[] = sprintf(
+                        "(
+                        ($t.training_center IN (
+                            SELECT tc.id
+                            FROM %s tc
+                            WHERE tc.title LIKE '%%%%%s%%%%'
+                            OR tc.city LIKE '%%%%%s%%%%'
+                        ))
+                        OR
+                        ($t.id IN (
+                            SELECT eso.pid
+                            FROM %s eso
+                            INNER JOIN %s ro ON eso.registration = ro.id
+                            INNER JOIN %s o ON ro.operator = o.id
+                            WHERE o.lastname LIKE '%%%%%s%%%%'
+                            OR o.firstname LIKE '%%%%%s%%%%'
+                        ))
+                        OR
+                        ($t.id IN (
+                            SELECT eso.pid
+                            FROM %s eso
+                            INNER JOIN %s ro ON eso.registration = ro.id
+                            INNER JOIN %s r ON ro.pid = r.id
+                            INNER JOIN %s c ON r.company = c.id
+                            WHERE c.title LIKE '%%%%%s%%%%'
+                        ))
+                        )",
+                        TrainingCenter::getTable(),
+                        $varValue,
+                        $varValue,
+                        ExamSessionOperator::getTable(),
+                        RegistrationOperator::getTable(),
+                        Operator::getTable(),
+                        $varValue,
+                        $varValue,
+                        ExamSessionOperator::getTable(),
+                        RegistrationOperator::getTable(),
+                        Registration::getTable(),
+                        Company::getTable(),
+                        $varValue,
+                    );
+                break;
+
+                // Load parent
+                default:
+                    $arrColumns = array_merge($arrColumns, parent::formatStatement($strField, $varValue, $strOperator));
+            }
+
+            return $arrColumns;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
 }
