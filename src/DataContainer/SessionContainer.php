01f2ac36cd92189acdca2309a0f373c7f618d0db
ChipsVII <benjamin@webexmachina.fr>
2023-07-25 13:50:12 +0200

    Feat : FQDN classnames

--- src/DataContainer/SessionContainer.php
+++ src/DataContainer/SessionContainer.php
@@ -2,9 +2,21 @@
 
 declare(strict_types=1);
 
+/**
+ * Geodata for Contao Open Source CMS
+ * Copyright (c) 2023 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-audiotracks
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-audiotracks/
+ */
+
 namespace WEM\AudioTracksBundle\DataContainer;
 
-class SessionContainer extends \Backend
+use Contao\Backend;
+
+class SessionContainer extends Backend
 {
     /**
      * Format items list.
