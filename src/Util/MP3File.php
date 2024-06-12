c46acfb3f5787502d172260e76a200599c18962a
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-06-12 09:45:13 +0200

    feat: Include return types in methods and replace arrays in MP3File class

--- src/Util/MP3File.php
+++ src/Util/MP3File.php
@@ -6,14 +6,14 @@ namespace WEM\AudioTracksBundle\Util;
 
 class MP3File
 {
-    protected $filename;
+    protected ?string $filename;
 
     public function __construct($filename)
     {
         $this->filename = $filename;
     }
 
-    public static function formatTime($duration) //as hh:mm:ss
+    public static function formatTime($duration): string //as hh:mm:ss
     {
         //return sprintf("%d:%02d", $duration/60, $duration%60);
         $hours = floor($duration / 3600);
@@ -67,7 +67,7 @@ class MP3File
         return round($duration);
     }
 
-    private function estimateDuration($bitrate,$offset)
+    private function estimateDuration($bitrate,$offset): float
     {
         $kbps = ($bitrate*1000)/8;
         $datasize = filesize($this->filename) - $offset;
@@ -101,31 +101,31 @@ class MP3File
         return 0;
     }
 
-    public static function parseFrameHeader($fourbytes)
+    public static function parseFrameHeader($fourbytes): array
     {
-        static $versions = array(
+        static $versions = [
             0x0=>'2.5',0x1=>'x',0x2=>'2',0x3=>'1', // x=>'reserved'
-        );
-        static $layers = array(
+        ];
+        static $layers = [
             0x0=>'x',0x1=>'3',0x2=>'2',0x3=>'1', // x=>'reserved'
-        );
-        static $bitrates = array(
-            'V1L1'=>array(0,32,64,96,128,160,192,224,256,288,320,352,384,416,448),
-            'V1L2'=>array(0,32,48,56, 64, 80, 96,112,128,160,192,224,256,320,384),
-            'V1L3'=>array(0,32,40,48, 56, 64, 80, 96,112,128,160,192,224,256,320),
-            'V2L1'=>array(0,32,48,56, 64, 80, 96,112,128,144,160,176,192,224,256),
-            'V2L2'=>array(0, 8,16,24, 32, 40, 48, 56, 64, 80, 96,112,128,144,160),
-            'V2L3'=>array(0, 8,16,24, 32, 40, 48, 56, 64, 80, 96,112,128,144,160),
-        );
-        static $sample_rates = array(
-            '1'   => array(44100,48000,32000),
-            '2'   => array(22050,24000,16000),
-            '2.5' => array(11025,12000, 8000),
-        );
-        static $samples = array(
-            1 => array( 1 => 384, 2 =>1152, 3 =>1152, ), //MPEGv1,     Layers 1,2,3
-            2 => array( 1 => 384, 2 =>1152, 3 => 576, ), //MPEGv2/2.5, Layers 1,2,3
-        );
+        ];
+        static $bitrates = [
+            'V1L1'=> [0,32,64,96,128,160,192,224,256,288,320,352,384,416,448],
+            'V1L2'=> [0,32,48,56, 64, 80, 96,112,128,160,192,224,256,320,384],
+            'V1L3'=> [0,32,40,48, 56, 64, 80, 96,112,128,160,192,224,256,320],
+            'V2L1'=> [0,32,48,56, 64, 80, 96,112,128,144,160,176,192,224,256],
+            'V2L2'=> [0, 8,16,24, 32, 40, 48, 56, 64, 80, 96,112,128,144,160],
+            'V2L3'=> [0, 8,16,24, 32, 40, 48, 56, 64, 80, 96,112,128,144,160],
+        ];
+        static $sample_rates = [
+            '1'   => [44100,48000,32000],
+            '2'   => [22050,24000,16000],
+            '2.5' => [11025,12000, 8000],
+        ];
+        static $samples = [
+            1 => [1 => 384, 2 =>1152, 3 =>1152,], //MPEGv1,     Layers 1,2,3
+            2 => [1 => 384, 2 =>1152, 3 => 576,], //MPEGv2/2.5, Layers 1,2,3
+        ];
         //$b0=ord($fourbytes[0]);//will always be 0xff
         $b1=ord($fourbytes[1]);
         $b2=ord($fourbytes[2]);
@@ -139,10 +139,10 @@ class MP3File
         $layer = $layers[$layer_bits];
         $bitrate_key = sprintf('V%dL%d', $simple_version , $layer);
         $bitrate_idx = ($b2 & 0xf0) >> 4;
-        $bitrate = isset($bitrates[$bitrate_key][$bitrate_idx]) ? $bitrates[$bitrate_key][$bitrate_idx] : 0;
+        $bitrate = $bitrates[$bitrate_key][$bitrate_idx] ?? 0;
 
         $sample_rate_idx = ($b2 & 0x0c) >> 2;//0xc => b1100
-        $sample_rate = isset($sample_rates[$version][$sample_rate_idx]) ? $sample_rates[$version][$sample_rate_idx] : 0;
+        $sample_rate = $sample_rates[$version][$sample_rate_idx] ?? 0;
         $padding_bit = ($b2 & 0x02) >> 1;
         $private_bit = ($b2 & 0x01);
         $channel_mode_bits = ($b3 & 0xc0) >> 6;
@@ -151,7 +151,7 @@ class MP3File
         $original_bit = ($b3 & 0x04) >> 2;
         $emphasis = ($b3 & 0x03);
 
-        $info = array();
+        $info = [];
         $info['Version'] = $version;//MPEGVersion
         $info['Layer'] = $layer;
         //$info['Protection Bit'] = $protection_bit; //0=> protected by 2 byte CRC, 1=>not protected
@@ -169,7 +169,7 @@ class MP3File
         return $info;
     }
 
-    private static function framesize($layer, $bitrate,$sample_rate,$padding_bit)
+    private static function framesize($layer, $bitrate,$sample_rate,$padding_bit): int
     {
         if ($layer==1) {
             return intval(((12 * $bitrate*1000 /$sample_rate) + $padding_bit) * 4);
