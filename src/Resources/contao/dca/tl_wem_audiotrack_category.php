9c7f597a68119173e946601057459a57813bcbad
LupusVII <jln.thirion@gmail.com>
2025-03-13 18:51:47 +0100

    add RSS fields

--- src/Resources/contao/dca/tl_wem_audiotrack_category.php
+++ src/Resources/contao/dca/tl_wem_audiotrack_category.php
@@ -9,6 +9,12 @@ $GLOBALS['TL_DCA']['tl_wem_audiotrack_category'] = [
         'ctable' => ['tl_wem_audiotrack'],
         'switchToEdit' => true,
         'enableVersioning' => true,
+        'onload_callback' => [
+            [WEM\AudioTracksBundle\DataContainer\CategoryContainer::class, 'displayRssUrl']
+        ],
+        'onsubmit_callback' => [
+            [WEM\AudioTracksBundle\DataContainer\CategoryContainer::class, 'generateRssFeed']
+        ],
         'sql' => [
             'keys' => [
                 'id' => 'primary',
@@ -59,10 +65,18 @@ $GLOBALS['TL_DCA']['tl_wem_audiotrack_category'] = [
 
     // Palettes
     'palettes' => [
+        '__selector__' => ['rss'],
         'default' => '
-            {title_legend},title,alias;
-            {settings_legend},tags
+            {title_legend},title,alias,categories,description;
+            {picture_legend},picture,pictureAlt,pictureTitle,pictureSize;
+            {settings_legend},language,tags;
+            {author_legend},authorName,authorEmail,authorUri;
+            {rss_legend},rss;
         ',
+    ],
+
+    'subpalettes' => [
+        'rss' => 'rssType,rssFilename,rssLink,rssHub,rssCopyright,rssDescription'
     ],
 
     // Fields
@@ -78,7 +92,6 @@ $GLOBALS['TL_DCA']['tl_wem_audiotrack_category'] = [
             'flag' => 8,
             'sql' => "int(10) unsigned NOT NULL default '0'",
         ],
-
         'title' => [
             'exclude' => true,
             'search' => true,
@@ -96,11 +109,132 @@ $GLOBALS['TL_DCA']['tl_wem_audiotrack_category'] = [
             ],
             'sql' => "varchar(255) BINARY NOT NULL default ''",
         ],
+        'categories' => [
+            'exclude' => true,
+            'inputType' => 'listWizard',
+            'eval' => ['tl_class'=>'clr', 'tl_class' => 'clr long'],
+            'sql' => "blob NULL"
+        ],
+        'description' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'textarea',
+            'eval' => ['mandatory' => true, 'rte' => 'tinyMCE', 'helpwizard' => true, 'tl_class' => 'clr'],
+            'explanation' => 'insertTags',
+            'sql' => 'mediumtext NULL',
+        ],
+        'picture' => [
+            'exclude' => true,
+            'inputType' => 'fileTree',
+            'eval' => ['filesOnly' => true, 'fieldType' => 'radio', 'tl_class' => 'clr', 'extensions' => Contao\Config::get('validImageTypes')],
+            'sql' => 'binary(16) NULL',
+        ],
+        'pictureAlt' => [
+            'exclude' => true,
+            'inputType' => 'text',
+            'eval' => array('maxlength'=>255, 'tl_class'=>'w50'),
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'pictureTitle' => [
+            'exclude' => true,
+            'inputType' => 'text',
+            'eval' => array('maxlength'=>255, 'tl_class'=>'w50'),
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'pictureSize' => [
+            'exclude' => true,
+            'inputType' => 'imageSize',
+            'reference' => &$GLOBALS['TL_LANG']['MSC'],
+            'eval' => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'),
+            'sql' => "varchar(128) COLLATE ascii_bin NOT NULL default ''"
+        ],
+        'language' => [
+            'exclude' => true,
+            'filter' => true,
+            'inputType' => 'select',
+            'eval'=> array('includeBlankOption'=>true, 'chosen'=>true, 'tl_class'=>'w50'),
+            'options_callback' => static function () {
+                return System::getContainer()->get('contao.intl.locales')->getLocales(null, false);
+            },
+            'sql' => "varchar(64) NOT NULL default ''"
+        ],
         'tags' => [
             'exclude' => true,
             'inputType' => 'listWizard',
             'eval' => ['tl_class'=>'clr'],
             'sql' => "blob NULL"
         ],
+        'authorName' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'authorEmail' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'authorUri' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'rss' => [
+            'exclude' => true,
+            'filter' => true,
+            'flag' => 1,
+            'inputType' => 'checkbox',
+            'eval' => ['doNotCopy' => true, 'submitOnChange' => true],
+            'sql' => "char(1) NOT NULL default ''",
+        ],
+        'rssType' => [
+            'exclude' => true,
+            'inputType' => 'select',
+            'options' => ['rss', 'atom'],
+            'eval' => ['tl_class'=>'clr'],
+            'sql' => "varchar(16) NOT NULL default ''"
+        ],
+        'rssFilename' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "text NULL"
+        ],
+        'rssLink' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'rssDescription' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'textarea',
+            'eval' => ['rte' => 'tinyMCE', 'helpwizard' => true, 'tl_class' => 'clr'],
+            'explanation' => 'insertTags',
+            'sql' => 'mediumtext NULL',
+        ],
+        'rssHub' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],
+        'rssCopyright' => [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class'=>'w50'],
+            'sql' => "varchar(255) NOT NULL default ''"
+        ],    
     ],
 ];
