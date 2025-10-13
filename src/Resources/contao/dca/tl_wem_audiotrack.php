b971060b9e6894d19c59428b96873271208b7baa
LupusVII <jln.thirion@ik.me>
2025-10-13 23:57:56 +0200

    Fix translation var

--- src/Resources/contao/dca/tl_wem_audiotrack.php
+++ src/Resources/contao/dca/tl_wem_audiotrack.php
@@ -57,7 +57,7 @@ $GLOBALS['TL_DCA']['tl_wem_audiotrack'] = [
             'delete' => [
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show' => [
                 'href' => 'act=show',
